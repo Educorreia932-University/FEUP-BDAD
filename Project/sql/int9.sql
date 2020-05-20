9e94ab9a9b23d9791605d5d9f8522c38750ee1dd
Educorreia932 <skelozard1@gmail.com>
2020-05-20 18:27:58 +0100

    9th query done

---
+++ Project/sql/int9.sql
@@ -0,0 +1,78 @@
+.mode columns.headers on.nullvalue NULL
+SELECT
+  *
+FROM (
+    (
+      (
+        SELECT
+          ID,
+          IFNULL(sendCount, 0) + IFNULL(rcvCount, 0) AS friendCount
+        FROM (
+            SELECT
+              senderID AS ID,
+              count(senderID) AS sendCount
+            FROM Friendship
+            WHERE
+              state IS 1
+            GROUP BY
+              senderID
+          )
+        LEFT JOIN (
+            SELECT
+              receiverID AS ID,
+              count(receiverID) AS rcvCount
+            FROM Friendship
+            WHERE
+              state IS 1
+            GROUP BY
+              receiverID
+          ) using(ID)
+        UNION
+        SELECT
+          ID,
+          IFNULL(sendCount, 0) + IFNULL(rcvCount, 0) AS nrFriends
+        FROM (
+            SELECT
+              receiverID AS ID,
+              count(receiverID) AS rcvCount
+            FROM Friendship
+            WHERE
+              state IS 1
+            GROUP BY
+              receiverID
+          )
+        LEFT JOIN (
+            SELECT
+              senderID AS ID,
+              count(senderID) AS sendCount
+            FROM Friendship
+            WHERE
+              state IS 1
+            GROUP BY
+              senderID
+          ) USING(ID)
+      ) -- Used to get amount of friends from all user
+      LEFT JOIN (
+          SELECT
+            participantID as ID,
+            count(eventID) AS eventCount
+          FROM EventParticipant
+          GROUP BY
+            ID
+        ) USING(ID)
+    ) --Number of events
+    LEFT JOIN (
+        SELECT
+          userID AS ID,
+          count(commentID) AS commentCount
+        FROM (
+            User
+            INNER JOIN (
+                Comment
+                INNER JOIN Post USING(postID)
+              ) AS postComment ON User.userID = postComment.publisherID
+          )
+        GROUP BY
+          userID
+      ) USING(ID) -- Number of comments in posts
+  );
