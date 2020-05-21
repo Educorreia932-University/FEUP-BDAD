d0b13d6b72b8d2a9f5daedb3363777f2b0da8af5
Educorreia932 <skelozard1@gmail.com>
2020-05-21 13:35:39 +0100

    Degrees of separation 0, 1 and 2 done for 10th query

--- Project/sql/int10.sql
+++ Project/sql/int10.sql
@@ -3,7 +3,7 @@
 .nullvalue NULL
 
 -- Friends of each user
-CREATE VIEW friends AS
+CREATE VIEW IF NOT EXISTS friends AS
     SELECT * FROM (
             (SELECT senderID as first, receiverID as second
                 FROM Friendship
@@ -14,18 +14,29 @@ CREATE VIEW friends AS
                     WHERE state IS 1)
     ) GROUP BY first, second;
 
--- One degree of separation
-SELECT f1.first, f1.second, COUNT(*) > 0
-    FROM friends AS f1
-        GROUP BY f1.first, f1.second;
+SELECT first, second, MIN(separationDegree) AS separationDegree FROM (
+    -- Zero degree of separation
+    SELECT f1.senderID AS first, f1.senderID AS second, 0 AS separationDegree
+        FROM Friendship AS f1
 
--- Two degrees of separation
-SELECT *
-    FROM Friendship AS f1
-        JOIN Friendship AS f2
-            ON f2.senderID = f1.receiverID;
+    UNION ALL
 
+    -- One degree of separation
+    SELECT f1.first, f1.second, 1 AS separationDegree
+        FROM friends AS f1
+            GROUP BY f1.first, f1.second
 
+    UNION ALL
+
+    -- Two degrees of separation
+    SELECT f1.first, f2.second, 2 AS separationDegree
+        FROM friends AS f1
+            JOIN friends AS f2
+                ON f2.first = f1.second 
+                    GROUP BY f1.first, f2.second
+
+    ORDER BY f1.first, f1.second
+) GROUP BY first, second
 
 
 
