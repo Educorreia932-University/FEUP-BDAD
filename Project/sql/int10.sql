4c727e4d3e6fa325b34e4d438ef1b7ada050693b
Educorreia932 <skelozard1@gmail.com>
2020-05-21 12:39:32 +0100

    Created a view in the 10th query to get all the friends of each user

--- Project/sql/int10.sql
+++ Project/sql/int10.sql
@@ -2,49 +2,31 @@
 .headers on
 .nullvalue NULL
 
--- SELECT * FROM (
---         SELECT senderID as first, receiverID as second
---             FROM Friendship
---                 WHERE state IS 1
---     UNION
---         SELECT receiverID as first, senderID as second
---             FROM Friendship
---                 WHERE state IS 1    
--- );
-
--- SELECT *, 1 FROM (
---         (SELECT senderID as first, receiverID as second
---             FROM Friendship
---                 WHERE state IS 1 AND senderID IS 5
---     UNION
---         SELECT receiverID as first, senderID as second
---             FROM Friendship
---                 WHERE state IS 1 AND receiverID IS 5)
---     INNER JOIN
---         (SELECT COUNT(*) FROM Friendship)
--- );
+-- Friends of each user
+CREATE VIEW friends AS
+    SELECT * FROM (
+            (SELECT senderID as first, receiverID as second
+                FROM Friendship
+                    WHERE state IS 1 
+        UNION
+            SELECT receiverID as first, senderID as second
+                FROM Friendship
+                    WHERE state IS 1)
+    ) GROUP BY first, second;
 
 -- One degree of separation
--- SELECT f1.senderID, f1.receiverID, COUNT(*) > 0
---     FROM Friendship AS f1
---         WHERE f1.state = 1
---             GROUP BY f1.senderID, f1.receiverID;
+SELECT f1.first, f1.second, COUNT(*) > 0
+    FROM friends AS f1
+        GROUP BY f1.first, f1.second;
 
 -- Two degrees of separation
--- SELECT *
---     FROM Friendship AS f1
---         JOIN Friendship AS f2
---             ON f2.senderID = f1.receiverID;
+SELECT *
+    FROM Friendship AS f1
+        JOIN Friendship AS f2
+            ON f2.senderID = f1.receiverID;
 
-SELECT * FROM (
-        (SELECT senderID as first, receiverID as second
-            FROM Friendship
-                WHERE state IS 1 
-    UNION
-        SELECT receiverID as first, senderID as second
-            FROM Friendship
-                WHERE state IS 1)
-) GROUP BY first, second;
+
+
 
 
 -- Three degrees
