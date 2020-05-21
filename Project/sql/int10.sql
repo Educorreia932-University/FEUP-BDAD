6e44289abce814b42d40b26f39fe18e0dcef9ae2
Educorreia932 <skelozard1@gmail.com>
2020-05-21 14:07:21 +0100

    10th query done

--- Project/sql/int10.sql
+++ Project/sql/int10.sql
@@ -5,47 +5,56 @@
 -- Friends of each user
 CREATE VIEW IF NOT EXISTS friends AS
     SELECT * FROM (
-            (SELECT senderID as first, receiverID as second
+            (SELECT senderID as source, receiverID as target
                 FROM Friendship
                     WHERE state IS 1 
         UNION
-            SELECT receiverID as first, senderID as second
+            SELECT receiverID as source, senderID as target
                 FROM Friendship
                     WHERE state IS 1)
-    ) GROUP BY first, second;
+    ) GROUP BY source, target;
 
-SELECT first, second, MIN(separationDegree) AS separationDegree FROM (
+SELECT source, target, MIN(separationDegree) AS separationDegree FROM (
     -- Zero degree of separation
-    SELECT f1.senderID AS first, f1.senderID AS second, 0 AS separationDegree
+    SELECT f1.senderID AS source, f1.senderID AS target, 0 AS separationDegree
         FROM Friendship AS f1
 
     UNION ALL
 
     -- One degree of separation
-    SELECT f1.first, f1.second, 1 AS separationDegree
+    SELECT f1.source, f1.target, 1 AS separationDegree
         FROM friends AS f1
-            GROUP BY f1.first, f1.second
+            GROUP BY f1.source, f1.target
 
     UNION ALL
 
     -- Two degrees of separation
-    SELECT f1.first, f2.second, 2 AS separationDegree
+    SELECT f1.source, f2.target, 2 AS separationDegree
         FROM friends AS f1
             JOIN friends AS f2
-                ON f2.first = f1.second 
-                    GROUP BY f1.first, f2.second
+                ON f2.source = f1.target 
+                    GROUP BY f1.source, f2.target
 
-    ORDER BY f1.first, f1.second
-) GROUP BY first, second
+    UNION ALL
 
+    -- Three degrees of separation
+    SELECT f1.source, f3.target, 3 AS separationDegree
+        FROM friends AS f1
+            JOIN friends AS f2
+                ON f2.source = f1.target
+                    JOIN friends AS f3
+                        ON f3.source = f2.target
+                            GROUP BY f1.source, f3.target
 
+    UNION ALL
 
--- Three degrees
--- SELECT *
---     FROM Friendship AS f1
---         JOIN Friendship AS f2 
---             ON f2.senderID = f1.receiverID
---                 JOIN Friendship AS f3
---                     ON f3.senderID = f2.receiverID
---                         WHERE f1.senderID = 10 AND f3.receiverID = 5;
+    -- Four degrees of separation or more
+    SELECT *, ">3" AS separationDegree 
+        FROM 
+            (SELECT userID AS source FROM User AS u1)
+        CROSS JOIN 
+            (SELECT userID AS target FROM User AS u2)
+
+    ORDER BY f1.source, f1.target
+) GROUP BY source, target
         
