7129eec15b7f261486c2e2c17ae00d1dc01cb3a3
Educorreia932 <skelozard1@gmail.com>
2020-05-22 17:33:18 +0100

    3rd query done

--- Project/sql/int3.sql
+++ Project/sql/int3.sql
@@ -9,9 +9,13 @@ CREATE VIEW m1 As
         FROM (Message NATURAL JOIN Multimedia) AS m1
             ORDER BY julianday(dateSent);
 
-SELECT messageID, min(dateSent - priorDateSent) AS hourDifference, size
-    FROM (SELECT t.messageID, t.dateSent, prior.dateSent AS priorDateSent, t.size
-        FROM m1 AS t
-            JOIN m1 AS prior ON (prior.dateSent < t.dateSent)
-    ) AS dates
-GROUP BY messageID;
+SELECT AVG(sizePerHour / 1024) as MBsPerHour FROM(
+    SELECT size / hourDifference AS sizePerHour FROM
+        (SELECT messageID, min(dateSent - priorDateSent) AS hourDifference, size
+            FROM (SELECT t.messageID, t.dateSent, prior.dateSent AS priorDateSent, t.size
+                FROM m1 AS t
+                    JOIN m1 AS prior ON (prior.dateSent < t.dateSent)
+            ) AS dates
+        GROUP BY messageID)
+    GROUP BY hourDifference, size
+);
