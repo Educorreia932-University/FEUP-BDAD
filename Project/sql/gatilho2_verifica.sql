0526a1ba00819baeb3ef2c5f4b39fa214ca2708f
ricardofontao2000 <50018106+ricardofontao2000@users.noreply.github.com>
2020-05-23 17:56:46 +0000

    Update gatilho2_verifica.sql

--- Project/sql/gatilho2_verifica.sql
+++ Project/sql/gatilho2_verifica.sql
@@ -2,6 +2,8 @@
 .headers on
 .nullvalue NULL
 
+PRAGMA FOREIGN_KEYS = ON;
+
 SELECT activityID, activityText, activityDate, publisherID FROM (Activity INNER JOIN Post ON Activity.activityID = Post.postID);
 
 INSERT INTO EventParticipant('participantID', 'eventID') VALUES(4,45);
